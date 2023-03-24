59f86e85a6bb97a4dcc062ce21a776286a201208
rohyapawar2912 <125944686+rohyapawar2912@users.noreply.github.com>
2023-03-24 16:19:41 +0530

    Add code

---
+++ partTimeWage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partTimeAdd
+{
+    internal class partTimeWage
+    {
+        public static void GetPartTimeWage()
+        {
+            int Work_days_permonth = Convert.ToInt32(Console.ReadLine());
+            int Wage_Per_Hr = Convert.ToInt32(Console.ReadLine());
+            int Hours_per_day = 8;
+
+            int HoursPerMonth = (Work_days_permonth * Hours_per_day);
+            int WagePerMonth = (Wage_Per_Hr * HoursPerMonth);
+            Console.WriteLine("Part time wage per month = " + WagePerMonth);
+        }
+    }
+}
